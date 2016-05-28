8b56834fdc4f66513e0d97a1ee2866c8b54d844b
chris <chris@localhost>
2016-05-28 12:55:12 +0200

    initial commit

---
+++ src/ELBRequest/ElbHeaderNotExistsException.php
@@ -0,0 +1,27 @@
+<?
+	namespace ELBRequest;
+
+	use Exception;
+
+	class ElbHeaderNotExistsException extends \Exception {
+
+		protected $header;
+
+		public function __construct($header, $message = "", $code = 0, Exception $previous = null) {
+
+			if (!$message)
+				$message = 'Header "' . $header . '" does not exist. Request seems not to be passed through an ELB instance';
+
+			parent::__construct($message, $code, $previous);
+		}
+
+		/**
+		 * @return string
+		 */
+		public function getHeader() {
+			return $this->header;
+		}
+
+
+
+	}
---
+++ src/ELBRequest/ElbMetaData.php
@@ -0,0 +1,81 @@
+<?
+	namespace ELBRequest;
+
+	/**
+	 * Gets ELB meta data about the requests. Forwarded protocol, remote ip and port
+	 * @package ELBRequest
+	 */
+	class ELBMetaData {
+
+		const FORWARDED_PROTOCOL_HTTP = 'http';
+		const FORWARDED_PROTOCOL_HTTPS = 'https';
+
+		protected function getElbHeader($headerName) {
+
+			// prepend prefix
+			$headerName = 'X-Forwarded-' . $headerName;
+
+			// get Server variable key
+			$key = 'HTTP_' . strtoupper(str_replace('-', '_', $headerName));
+
+			// check existence
+			if (!array_key_exists($key, $_SERVER))
+				throw new ElbHeaderNotExistsException($headerName);
+
+			return $_SERVER[$key];
+		}
+
+		/**
+		 * Gets the port the client used to connect to the ELB
+		 * @return int The port (e.g. 80 or 443)
+		 * @throws ElbHeaderNotExistsException
+		 */
+		public static function getForwardedPort() {
+			return self::getElbHeader('Port');
+		}
+
+		/**
+		 * Gets the protocol the client used to connect to the ELB
+		 * @return string The protocol (e.g. 'https' or 'http')
+		 * @throws ElbHeaderNotExistsException
+		 */
+		public static function getForwardedProtocol() {
+			return self::getElbHeader('Proto');
+		}
+
+		/**
+		 * Gets the IPs of all proxies and the client within the forwarding chain.
+		 * @return string[] The ips of all proxies and the client. The last address is the address of the most recent proxy which connected to the ELB.
+		 * @throws ElbHeaderNotExistsException
+		 */
+		public static function getForwardedClientIPs() {
+			return explode(',', str_replace(' ', '', self::getElbHeader('For')));
+		}
+
+		/**
+		 * Checks if the client connected via HTTPS to the ELB
+		 * @return bool True if connected via HTTPS. Else false
+		 * @throws ElbHeaderNotExistsException
+		 */
+		public static function isHttps() {
+			return self::getForwardedProtocol() == self::FORWARDED_PROTOCOL_HTTPS;
+		}
+
+		/**
+		 * Checks if the client connected via HTTP to the ELB
+		 * @return bool True if connected via HTTP. Else false
+		 * @throws ElbHeaderNotExistsException
+		 */
+		public static function isHttp() {
+			return self::getForwardedProtocol() == self::FORWARDED_PROTOCOL_HTTP;
+		}
+
+		/**
+		 * Gets the IP of the the most recent proxy which connected to the ELB. This equals the remote address.
+		 * @return string The remote IP. (eg. '203.0.113.7' or '2001:DB8::21f:5bff:febf:ce22:8a2e')
+		 */
+		public static function getRemoteIp() {
+			return array_pop(self::getForwardedClientIPs());
+		}
+
+	}
